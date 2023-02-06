bb88b8b8de94d310e26dbd53a60ce9508aa307b1
Arix <91370301+XxArixxX@users.noreply.github.com>
2023-02-06 20:53:36 +0300

    Add files via upload

---
+++ ParserV2/ParserV2/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+
+namespace ParserV2
+{
+    public partial class Form1 : Form
+    {
+        private string filePath = "";
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Enabled = false;
+            button2.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog.FileName;
+                LoadExcelData();
+            }
+        }
+
+        private void LoadExcelData()
+        {
+            string connectionString = "";
+
+            if (filePath.EndsWith(".xls"))
+            {
+                connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filePath + ";Extended Properties='Excel 8.0;HDR=YES;IMEX=1;'";
+            }
+            else if (filePath.EndsWith(".xlsx") || filePath.EndsWith(".xlsm"))
+            {
+                connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties='Excel 12.0 Xml;HDR=YES;IMEX=1;'";
+            }
+
+            OleDbConnection connection = new OleDbConnection(connectionString);
+            OleDbCommand command = new OleDbCommand($"Select * from [{comboBox1.SelectedItem}$]", connection);
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(command);
+            System.Data.DataTable dataTable = new System.Data.DataTable();
+
+            try
+            {
+                connection.Open();
+                dataAdapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+                button2.Enabled = true;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex != -1)
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "JSON Files|*.json";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string json = JsonConvert.SerializeObject(dataGridView1.DataSource, Formatting.Indented);
+                System.IO.File.WriteAllText(saveFileDialog.FileName, json);
+                MessageBox.Show("JSON файл успешно сохранен.");
+            }
+        }
+    }
+}
